991c266dd3e14cca8821c569102d6bb96e7a6b7c
Christian González Di Antonio <christiangda@gmail.com>
2019-05-04 23:21:23 +0200

    added testinfra tests

--- molecule/vagrant/tests/test_ssm_agent.py
+++ molecule/vagrant/tests/test_ssm_agent.py
@@ -7,13 +7,28 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_amazon_ssm_agent_is_installed(host):
-    pkg = host.package('amazon-ssm-agent')
+    os = host.system_info.distribution.lower()
+    rl = host.system_info.release
 
-    assert pkg.is_installed
+    # Ubuntu is installed using snap package
+    if os == 'ubuntu' and rl == '16.04':
+        f = host.file('/snap/amazon-ssm-agent/current/amazon-ssm-agent')
+        assert f.exists
+        assert f.user == 'root'
+        assert f.group == 'root'
+    else:
+        pkg = host.package('amazon-ssm-agent')
+        assert pkg.is_installed
 
 
 def test_amazon_ssm_agent_running_and_enabled(host):
-    amazon_ssm_agent = host.service('amazon-ssm-agent')
+    os = host.system_info.distribution.lower()
+    rl = host.system_info.release
 
-    assert amazon_ssm_agent.is_running
-    assert amazon_ssm_agent.is_enabled
+    if (os == 'centos' and rl.split('.')[0] == '6') or (os == 'ubuntu' and rl == '16.04'):
+        p = host.process.get(user="root", comm="amazon-ssm-agen")
+        assert p is not None
+    else:
+        amazon_ssm_agent = host.service('amazon-ssm-agent')
+        assert amazon_ssm_agent.is_running
+        assert amazon_ssm_agent.is_enabled
